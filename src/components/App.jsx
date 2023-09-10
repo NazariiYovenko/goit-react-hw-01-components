0f48657ce37e78cd6fbdcad8799920d2c56feea1
Nazarii Yovenko <106098466+NazariiYovenko@users.noreply.github.com>
2023-09-10 22:51:13 +0300

    Add components
    * Add folders for components
    * Create jsx files for components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
 import Profile from './Profile/Profile';
 import user from './data/user.json';
+
+import Statistics from './Statistics/Statistics';
+import data from './data/data.json';
+
 // import friends from './data/friends.json';
 // import transactions from './data/transactions.js';
-// import data from './data/data.json';
 
 export const App = () => {
   return (
@@ -23,6 +26,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
   );
 };
