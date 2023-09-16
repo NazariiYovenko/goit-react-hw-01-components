625caf9da506e41e2b9b00cd7d420d5086eb2c62
Nazarii Yovenko <106098466+NazariiYovenko@users.noreply.github.com>
2023-09-16 04:18:01 +0300

    FriendsList and FriendsListItem jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,8 @@ import user from './data/user.json';
 import Statistics from './Statistics/Statistics';
 import data from './data/data.json';
 
-// import friends from './data/friends.json';
+import friends from './data/friends.json';
+import FriendList from './FriendList/FriendList';
 // import transactions from './data/transactions.js';
 
 export const App = () => {
@@ -26,9 +27,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import FriendListItem from 'components/FriendListItem/FriendListItem';
+import { arrayOf } from 'prop-types';
+export default function FriendList({ friends }) {
+  return (
+    <ul className="friend-list">
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          isOnline={isOnline}
+          name={name}
+        />
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+};
