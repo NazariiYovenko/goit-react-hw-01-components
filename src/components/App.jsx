97d24dd28dd45e02f1d8ecdaced470dd159845f8
Nazarii Yovenko <106098466+NazariiYovenko@users.noreply.github.com>
2023-09-16 04:21:19 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,7 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        color: '#010101',
+        color: '#010102',
       }}
     >
       <Profile
